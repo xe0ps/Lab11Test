14dfc5d2d9e8e7c9a49df09918c72d8556f16b97
xe0ps <101919209+xe0ps@users.noreply.github.com>
2023-11-14 13:11:29 +0200

    Update TestCases.kt

--- TestCases.kt
+++ TestCases.kt
@@ -2,45 +2,33 @@ class TestCases {
 
     @Test
     fun testValidRegistration() {
-        val result = authenticateUser("xeops", 19, "12345678")
-        if (result == 1) {
-            println("TC1: Passed = 1")
-        } else {
-            println("TC1: Failed")
+        ActivityScenario.launch(MainActivity::class.java).onActivity { mainActivity ->
+            val result = mainActivity.authenticateUser("xeops", 19, "12345678")
+            assertEquals(1, result)
         }
-        assertEquals(1, result)
     }
 
     @Test
     fun testInvalidLogin() {
-        val result = authenticateUser("xeopsssssssssssssssssssssantaaa", 19, "12345678")
-        if (result == -1) {
-            println("TC2: Passed = -1")
-        } else {
-            println("TC2: Failed")
+        ActivityScenario.launch(MainActivity::class.java).onActivity { mainActivity ->
+            val result = mainActivity.authenticateUser("xeopsssssssssssssssssssssantaaa", 19, "12345678")
+            assertEquals(-1, result)
         }
-        assertEquals(-1, result)
     }
 
     @Test
     fun testInvalidAge() {
-        val result = authenticateUser("xeops", 5, "12345678")
-        if (result == -2) {
-            println("TC3: Passed = -2")
-        } else {
-            println("TC3: Failed")
+        ActivityScenario.launch(MainActivity::class.java).onActivity { mainActivity ->
+            val result = mainActivity.authenticateUser("xeops", 5, "12345678")
+            assertEquals(-2, result)
         }
-        assertEquals(-2, result)
     }
 
     @Test
     fun testInvalidPassword() {
-        val result = authenticateUser("xeops", 19, "12345678123456781234567812345678")
-        if (result == -3) {
-            println("TC4: Passed = -3")
-        } else {
-            println("TC4: Failed")
+        ActivityScenario.launch(MainActivity::class.java).onActivity { mainActivity ->
+            val result = mainActivity.authenticateUser("xeops", 19, "12345678123456781234567812345678")
+            assertEquals(-3, result)
         }
-        assertEquals(-3, result)
     }
 }
