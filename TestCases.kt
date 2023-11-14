05b4c2533e78dd8407c0c29d35f1ccd91719fc74
xe0ps <101919209+xe0ps@users.noreply.github.com>
2023-11-14 09:09:18 +0200

    Update TestCases.kt

--- TestCases.kt
+++ TestCases.kt
@@ -1,35 +1,26 @@
-@RunWith(AndroidJUnit4::class)
 class TestCases {
 
     @Test
     fun testValidRegistration() {
-        ActivityScenario.launch(MainActivity::class.java).onActivity { mainActivity ->
-            val result = mainActivity.authenticateUser("xeops", 19, "12345678")
-            assertEquals(1, result)
-        }
+        val result = authenticateUser("xeops", 19, "12345678")
+        assertEquals(1, result)
     }
 
     @Test
     fun testInvalidLogin() {
-        ActivityScenario.launch(MainActivity::class.java).onActivity { mainActivity ->
-            val result = mainActivity.authenticateUser("xeopsssssssssssssssssssssantaaa", 19, "12345678")
-            assertEquals(-1, result)
-        }
+        val result = authenticateUser("xeopsssssssssssssssssssssantaaa", 19, "12345678")
+        assertEquals(-1, result)
     }
 
     @Test
     fun testInvalidAge() {
-        ActivityScenario.launch(MainActivity::class.java).onActivity { mainActivity ->
-            val result = mainActivity.authenticateUser("xeops", 5, "12345678")
-            assertEquals(-2, result)
-        }
+        val result = authenticateUser("xeops", 5, "12345678")
+        assertEquals(-2, result)
     }
 
     @Test
     fun testInvalidPassword() {
-        ActivityScenario.launch(MainActivity::class.java).onActivity { mainActivity ->
-            val result = mainActivity.authenticateUser("xeops", 19, "12345678123456781234567812345678")
-            assertEquals(-3, result)
-        }
+        val result = authenticateUser("xeops", 19, "12345678123456781234567812345678")
+        assertEquals(-3, result)
     }
 }
