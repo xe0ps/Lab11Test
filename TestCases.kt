21e4a01f7eb71bfbc4e9a839fc91b10393ca49de
xe0ps <101919209+xe0ps@users.noreply.github.com>
2023-11-14 08:51:21 +0200

    Create TestCases.kt

---
+++ TestCases.kt
@@ -0,0 +1,33 @@
+@RunWith(AndroidJUnit4::class)
+class MainActivityTest {
+    @Test
+    fun testValidRegistration() {
+        ActivityScenario.launch(MainActivity::class.java).onActivity { mainActivity ->
+            val result = mainActivity.authenticateUser("xeops", 19, "12345678")
+            assertEquals(1, result)
+        }
+    }
+    @Test
+    fun testInvalidLogin() {
+        ActivityScenario.launch(MainActivity::class.java).onActivity { mainActivity ->
+            val result = mainActivity.authenticateUser("xeopsssssssssssssssssssssantaaa", 19, "12345678")
+            assertEquals(-1, result)
+        }
+    }
+
+    @Test
+    fun testInvalidAge() {
+        ActivityScenario.launch(MainActivity::class.java).onActivity { mainActivity ->
+            val result = mainActivity.authenticateUser("xeops", 5, "12345678")
+            assertEquals(-2, result)
+        }
+    }
+
+    @Test
+    fun testInvalidPassword() {
+        ActivityScenario.launch(MainActivity::class.java).onActivity { mainActivity ->
+            val result = mainActivity.authenticateUser("xeops", 19, "12345678123456781234567812345678")
+            assertEquals(-3, result)
+        }
+    }
+}
