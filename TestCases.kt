e8ea7982bf188b2275a5d8d93a9379964790d322
xe0ps <101919209+xe0ps@users.noreply.github.com>
2023-11-15 17:30:46 +0200

    Update TestCases.kt

--- TestCases.kt
+++ TestCases.kt
@@ -24,17 +24,17 @@ object TestCases {
           if (result1 == 1) "Passed = 1" else "Failed"
       }")
 
-      val result2 = testCases.authenticateUser("xeopsssssssssssssssssssssantaaa", 19, "12345678")
+      val result2 = authenticateUser("xeopsssssssssssssssssssssantaaa", 19, "12345678")
       println("TC2: ${
           if (result2 == -1) "Passed = -1" else "Failed"
       }")
 
-      val result3 = testCases.authenticateUser("xeops", 5, "12345678")
+      val result3 = authenticateUser("xeops", 5, "12345678")
       println("TC3: ${
           if (result3 == -2) "Passed = -2" else "Failed"
       }")
 
-      val result4 = testCases.authenticateUser("xeops", 19, "12345678123456781234567812345678")
+      val result4 = authenticateUser("xeops", 19, "12345678123456781234567812345678")
       println("TC4: ${
           if (result4 == -3) "Passed = -3" else "Failed"
     }")
