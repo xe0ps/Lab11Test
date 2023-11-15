848f4589cdf786400b1e2c2df0aee1ebc18caf41
xe0ps <101919209+xe0ps@users.noreply.github.com>
2023-11-15 07:34:13 +0200

    Update UserAuthenticator.kt

--- UserAuthenticator.kt
+++ UserAuthenticator.kt
@@ -1,15 +1,31 @@
 class UserAuthenticator {
     fun authenticateUser(username: String, age: Int, password: String): Int {
         if (username.length > 20) {
-            return -1 // Invalid username
+            return -1 // Неверное имя пользователя
         }
         if (age < 6 || age > 120) {
-            return -2 // Invalid age
+            return -2 // Неверный возраст
         }
         if (password.length < 8 || password.length > 32) {
-            return -3 // Invalid password length
+            return -3 // Неверная длина пароля
         }
-        // Simulating authentication logic (returning 1 for valid authentication)
+        // Симуляция логики аутентификации (возвращается 1 для успешной аутентификации)
         return 1
     }
+}
+
+fun main() {
+    val userAuthenticator = UserAuthenticator()
+
+    val result1 = userAuthenticator.authenticateUser("xeops", 19, "12345678")
+    println("Результат теста 1: $result1")
+
+    val result2 = userAuthenticator.authenticateUser("xeopsssssssssssssssssssssantaaa", 19, "12345678")
+    println("Результат теста 2: $result2")
+
+    val result3 = userAuthenticator.authenticateUser("xeops", 5, "12345678")
+    println("Результат теста 3: $result3")
+
+    val result4 = userAuthenticator.authenticateUser("xeops", 19, "12345678123456781234567812345678")
+    println("Результат теста 4: $result4")
 }
